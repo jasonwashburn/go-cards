cefde5546d7b682e644581feba6d486bed7b3144
Jason Washburn <jason@shyftsolutions.io>
2022-01-19 11:10:22 +0000

    :sparkles: adds toString function

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
-
-	hand, remainingCards := deal(cards, 4)
-
-	hand.print()
-	remainingCards.print()
+	fmt.Println(cards.toString())
 }
