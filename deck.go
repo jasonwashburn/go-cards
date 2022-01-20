a6d4b2c55f232b227399be130071ec2e4e1836a9
Jason Washburn <jason@shyftsolutions.io>
2022-01-20 10:08:32 +0000

    :sparkles: adds saveToFile

--- deck.go
+++ deck.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"strings"
 )
 
@@ -34,4 +35,8 @@ func (d deck) print() {
 
 func (d deck) toString() string {
 	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
